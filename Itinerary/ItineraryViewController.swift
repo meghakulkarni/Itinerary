f34e3e3606b5e73d0a15f1a8c57319e4295c8a09
Megha Kulkarni <mkulkarni1991@gmail.com>
2019-09-24 16:09:16 +0530

    Itinerary app complete with datasource and delegate methods.

---
+++ Itinerary/ItineraryViewController.swift
@@ -0,0 +1,39 @@
+
+
+import UIKit
+
+class ItineraryViewController: UITableViewController {
+    
+
+    let itemArray = ["Buy Veggies", "Go for walk", "Clean home"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItineraryItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
