8022ea61273711a02ac10be27b52c9b798e566aa
Megha Kulkarni <mkulkarni1991@gmail.com>
2019-09-24 13:01:58 +0530

    Initial Commit

---
+++ Itinerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itinerary
+//
+//  Created by Megha Kulkarni on 24/09/19.
+//  Copyright © 2019 Megha Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
